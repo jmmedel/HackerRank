1d813f0f71b809e065a2aadb3e75e83a7d1b3b49
Shivam Sharma <53265299+shivamksharma@users.noreply.github.com>
2019-07-27 00:59:52 +0530

    Update 5_Functions.cpp

--- C++/1_Introduction/5_Functions.cpp
+++ C++/1_Introduction/5_Functions.cpp
@@ -95,3 +95,7 @@ int main() {
     printf("%d", ans);
     return 0;
 }
+
+
+/* NOTE - Dont Just copy the code because taking a copy of existing code in your project and repurposing it – violates coding best practices like Don’t Repeat Yourself (DRY). It’s one of the most cited examples of technical debt, a lazy way of working, sloppy and short-sighted: an antipattern that adds to the long term cost of keeping a code base alive. Copying and pasting code (“copy-paste”) is a primitive but very common form of software re-use. Unfortunately, this practice is fraught with dangers not least of which is duplicating bugs and security defects throughout the system.
+*/
