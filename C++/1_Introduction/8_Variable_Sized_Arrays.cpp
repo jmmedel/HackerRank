23dcbfdd6b6df8278a1eae8bde3dd8f22313ec92
Shivam Sharma <53265299+shivamksharma@users.noreply.github.com>
2019-07-27 01:25:06 +0530

    Update 8_Variable_Sized_Arrays.cpp

--- C++/1_Introduction/8_Variable_Sized_Arrays.cpp
+++ C++/1_Introduction/8_Variable_Sized_Arrays.cpp
@@ -1,3 +1,18 @@
+// https://www.hackerrank.com/challenges/8_Variable_Sized_Arrays.cpp
+/* The first line contains two space-separated integers denoting the respective values of n (the number of variable-length arrays) and q (the number of queries).
+Each line i of the n subsequent lines contains a space-separated sequence in the format k a[i]0 a[i]1 … a[i]k-1 describing the k -element array located at a[i].
+Each of the q subsequent lines contains two space-separated integers describing the respective values of i (an index in array a ) and j (an index in the array referenced by a[i] ) for a query.
+*/
+
+------------------------------------------------------------------------------------------------------------------------
+    
+CODE :-
+
+// https://www.hackerrank.com/challenges/8_Variable_Sized_Arrays.cpp
+/* Made by SHIVAM SHARMA */
+
+
+
 #include <vector>
 #include <iostream>
 using namespace std;
@@ -27,3 +42,5 @@ int main()
     }
     return 0;
 }
+
+/* NOTE - Dont Just copy the code because taking a copy of existing code in your project and repurposing it – violates coding best practices like Don’t Repeat Yourself (DRY). It’s one of the most cited examples of technical debt, a lazy way of working, sloppy and short-sighted: an antipattern that adds to the long term cost of keeping a code base alive. Copying and pasting code (“copy-paste”) is a primitive but very common form of software re-use. Unfortunately, this practice is fraught with dangers not least of which is duplicating bugs and security defects throughout the system.*/
